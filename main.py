562b7ecc26ebc95822ab4ed295dc772a1168b92e
JayShepherdDhani <jay.shepherd@dhani.com>
2021-08-20 11:48:10 +0100

    include main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from backend.core.config import settings
+from backend.db.session import engine
+from backend.db.base_class import Base
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
