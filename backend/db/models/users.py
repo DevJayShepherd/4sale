b671ee77d651736ca50a650ce2175bd5186803fc
JayShepherdDhani <jay.shepherd@dhani.com>
2021-08-22 21:28:48 +0100

    Create db models, schema for validation and hasher function for password protection.

---
+++ backend/db/models/users.py
@@ -0,0 +1,21 @@
+import datetime
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, Date
+from sqlalchemy.orm import relationship
+
+from backend.db.base_class import Base
+
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(255), nullable=False)
+    last_name = Column(String(255), nullable=False)
+    hashed_password = Column(String(255), nullable=False)
+    email = Column(String(255), nullable=False, unique=True)
+    facebook = Column(String, nullable=True)
+    twitter = Column(String, nullable=True)
+    website = Column(String, nullable=True)
+    phone = Column(String, nullable=True)
+    public_email = Column(String, nullable=True)
+    picture = Column(String, nullable=True)
+    date_joined = Column(Date, default=datetime.datetime.now)
+    property = relationship("Property", back_populates="owner")
--- backend/db/session.py
+++ backend/db/session.py
@@ -2,12 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.core.config import settings
+from typing import Generator
 
 engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
-
-
-
+def get_db() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
