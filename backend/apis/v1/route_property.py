088ff2d1522856be1b2b6a89a01f65685bd97f65
Jay Shepherd <69314244+DevJayShepherd@users.noreply.github.com>
2021-08-25 15:51:53 +0100

    routes added for retrieving list of properties from db and editing a property by id, also common schema file started for generic responses. (#6)
    Co-authored-by: JayShepherdDhani <jay.shepherd@dhani.com>

--- backend/apis/v1/route_property.py
+++ backend/apis/v1/route_property.py
@@ -1,16 +1,33 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
+from starlette import status
 
 from backend.db.session import get_db
-from backend.db.models.property import Property
-from backend.db.repository.properties import add_new_property
+from backend.db.repository.properties import add_new_property, retrieve_properties, update_property_by_id
 from backend.schema.properties import PropertyCreate, PropertyShow
+from backend.schema.common import DetailResponse
+from typing import List
 
 router = APIRouter()
 
 
-@router.post("/create-property", response_model=PropertyShow)
+@router.post("/create_property", response_model=PropertyShow)
 def add_property(the_property: PropertyCreate, db: Session = Depends(get_db)):
     owner_id = 1
     the_property = add_new_property(the_property, db, owner_id=owner_id)
     return the_property
+
+
+@router.get("/retrieve_all", response_model=List[PropertyShow])
+def retrieve_all_properties(db: Session = Depends(get_db)):
+    properties = retrieve_properties(db)
+    return properties
+
+
+@router.put("edit_property/{id}", response_model=DetailResponse)
+def edit_the_property(id: int, the_property: PropertyCreate, db: Session = Depends(get_db)):
+    owner_id = 1
+    response = update_property_by_id(property_id=id, the_property=the_property, db=db, owner_id=owner_id)
+    if not response:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Job with id: {id} does not exist.")
+    return {"detail": "Successfully updated."}
