12f711cd79801d01598c5ee7a970a3c9f5632902
JayShepherdDhani <jay.shepherd@dhani.com>
2021-08-28 16:45:14 +0100

    added linter

--- backend/db/base.py
+++ backend/db/base.py
@@ -1,4 +1,3 @@
 from backend.db.base_class import Base
 from backend.db.models.property import Property
 from backend.db.models.users import User
-
--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,14 +1,15 @@
+import sys
+import os
+import pytest
+
+
 from typing import Any
 from typing import Generator
 
-import pytest
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-import sys
-import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # this is to include backend dir in sys.path so that we can import from db,main.py
